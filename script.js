6d05db2f4b95dd0058a4f9c179b42b140170552d
Luke Armstrong <ltarmstrong96@gmail.com>
2020-02-17 17:16:43 +1300

    Changed Gallery
    Added to Gallery

--- script.js
+++ script.js
@@ -26,3 +26,35 @@ function overlayOn(x) {
 function overlayOff(x) {
     document.getElementById(x).style.display = "none";
 }
+
+var galleryIndex = 0;
+var timer;
+
+showImage(0);
+
+function showImage(imgNum) {
+  clearInterval( timer );
+
+  var move_left = imgNum * -100;
+  var dotId = "dot-" + imgNum.toString();
+  var dots = document.getElementsByClassName("slider-dot")
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].style.backgroundColor = "transparent";
+  }
+
+  document.getElementById(dotId).style.backgroundColor = "white";
+  document.getElementById("gallery-slides").style.left = move_left + "%";
+
+  galleryIndex = imgNum;
+
+  var nextIndex;
+  if ( ( galleryIndex + 1 ) == dots.length )
+  {
+    nextIndex = 0;
+  }
+  else {
+    nextIndex = galleryIndex + 1;
+  }
+  timer = setInterval( showImage, 8000, nextIndex );
+}
